de8008773d662493105bfd4a9bde53ca324e9753
pat-coady <pcoady00@gmail.com>
2024-05-28 08:13:40 -0600

    Fix errors in setup script

--- setup.sh
+++ setup.sh
@@ -1,9 +1,13 @@
-sudo apt update
 # remove this plugin that GCP includes in jupyter install
 # causes a bunch of warnings
 pip uninstall dataproc_jupyter_plugin
+
+# clone repo and install python packages
 git clone https://github.com/pat-coady/hf-sandbox.git
 pip install -r requirements.txt
+
 # point to jupyter config in working directory
 # sets ip = '*', port='8888', open_browser=False
 export JUPYTER_CONFIG_DIR="."
+# add startup script for next time
+echo 'export JUPYTER_CONFIG_DIR="."' >> ~/.bashrc
